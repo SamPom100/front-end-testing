995772439cccfb2e47f142dc667ad9c0b1607ac7
Sam Pomerantz <sam@sampomerantz.me>
2022-05-03 23:55:18 -0400

    init

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import webbrowser
+app = Flask(__name__)
+
+
+
+
+
+@app.route("/")
+def hello_world():
+  return render_template('home.html') 
+
+if __name__ == "__main__":
+    webbrowser.open_new('http://127.0.0.1:5002/')
+    app.run(port=5000, debug=True, use_reloader=False)
